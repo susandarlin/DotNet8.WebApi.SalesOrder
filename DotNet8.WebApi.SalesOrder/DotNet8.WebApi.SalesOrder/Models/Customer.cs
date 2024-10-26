d660b78ab90ae51e73060052966e5647291e60b8
Sandar <sandar@solidplm.com>
2024-10-26 17:44:58 +0700

    Add Customer and Stock Models

---
+++ DotNet8.WebApi.SalesOrder/DotNet8.WebApi.SalesOrder/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNet8.WebApi.SalesOrder.Models
+{
+    public class Customer
+    {
+        public Guid customerId { get; set; }
+        public string? customerName { get; set; }
+        public int customerPhoneNo { get; set; }
+        public string? customerAddress { get; set; }
+    }
+}
---
+++ DotNet8.WebApi.SalesOrder/DotNet8.WebApi.SalesOrder/Models/Stock.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNet8.WebApi.SalesOrder.Models
+{
+    public class Stock
+    {
+        public Guid stockId { get; set; }
+        public string? stockName { get; set; }
+        public string? stockDescription { get; set; }
+        public float stockPrice { get; set; }
+        public int stockOnHandQty { get; set; }
+    }
+}
