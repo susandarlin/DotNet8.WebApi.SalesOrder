e15e237093ea7df5708e14b42ec458c62d76b26b
Sandar <sandar@solidplm.com>
2024-10-26 17:50:33 +0700

    Add Sale Order Model

---
+++ DotNet8.WebApi.SalesOrder/DotNet8.WebApi.SalesOrder/Models/SaleOrder.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNet8.WebApi.SalesOrder.Models
+{
+    public class SaleOrder
+    {
+        public Guid soId { get; set; }
+        public Customer? customer { get; set; }
+        public List<Stock>? stocks { get; set; }
+        public DateTime soDate { get; set; }
+    }
+}
